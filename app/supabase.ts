50be39091cf45c138a2904baaf124416e6c8b1a5
Patricio <tu@email.com>
2026-06-14 22:10:44 -0400

    fix supabase url build

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://pcfrezymophbunibqmmk.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_wCq5RHh680zalPe28DB0KA_RmTzwX2c'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
